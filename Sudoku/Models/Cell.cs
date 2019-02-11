90d80731bd592494a584bc008ac27504ee9e988f
Taylor Rosby <taylorrosby@gmail.com>
2019-02-11 16:15:51 -0700

    Cell class

---
+++ Sudoku/Models/Cell.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sudoku.Models
+{
+    public class Cell
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public int? value { get; set; }
+        public int blockNumber { get; set; }
+        public bool marked { get; set; }
+
+        public Cell()
+        {
+            value = null;
+        }
+
+    }
+}
