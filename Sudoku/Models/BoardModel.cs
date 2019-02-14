240bbad861aa2f9da030cb49aa21b98aa239808b
Taylor Rosby <taylorrosby@gmail.com>
2019-02-14 14:36:52 -0700

    Board Model

---
+++ Sudoku/Models/BoardModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sudoku.Models
+{
+    public class BoardModel
+    {
+        public List<Cell> GameBoard { get; set; }
+
+        public BoardModel()
+        {
+            GameBoard = new List<Cell>();
+        }
+    }
+}
